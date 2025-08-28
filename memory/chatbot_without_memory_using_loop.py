f88d2623e4f37d34606f7d0a5bb3510bacc0590d
riteshhub <patel.ritesh0102@gmail.com>
2025-08-28 11:49:00 +0530

    add multiple exercises for chatbot

---
+++ memory/chatbot_without_memory_using_loop.py
@@ -0,0 +1,47 @@
+from dotenv import load_dotenv
+from typing import Annotated, TypedDict
+from langchain_core.messages import HumanMessage, AIMessage
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.output_parsers import StrOutputParser
+from langgraph.graph import StateGraph, START, END
+from langgraph.graph.message import add_messages
+
+load_dotenv()
+
+llm = ChatGoogleGenerativeAI(
+    model="gemini-2.0-flash",
+    temperature=0.1,
+    max_output_tokens=1024
+)
+
+parser = StrOutputParser()
+
+# define state
+class ChatState(TypedDict):
+    messages: Annotated[list[str], add_messages]
+
+# define methods
+def chat(state: ChatState):
+    message = state["messages"]
+    chain = llm | parser
+    result = chain.invoke(message)
+    return {"messages": [AIMessage(content=result)]}
+
+# define graph
+graph = StateGraph(ChatState)
+
+# define nodes
+graph.add_node("chat", chat)
+
+# define edges
+graph.add_edge(START, "chat")
+graph.add_edge("chat", END)
+
+chatbot = graph.compile()
+
+while True:
+    user_input = input("You: ")
+    if user_input.lower() in ["exit", "quit"]:
+        break
+    result = chatbot.invoke({"messages": [HumanMessage(content=user_input)]})
+    print(f"AI: {result['messages'][-1].content}")
